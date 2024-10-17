f4dceafd492b342b85764b6833c5c9b54bff61da
Volodymyr Chekyrta <volodymyr.chekyrta@raccoongang.com>
2024-10-17 16:40:20 +0300

    Add static lib

--- Package.swift
+++ Package.swift
@@ -15,18 +15,23 @@ let package = Package(
             type: .dynamic,
             targets: ["MyPluginForAbstraction"]
         ),
+        .library(
+            name: "MyPluginForAbstraction_Static",
+            type: .static,
+            targets: ["MyPluginForAbstraction"]
+        )
     ],
     dependencies: [
         .package(
             url: "https://github.com/volodymyr-chekyrta/iOSReusableToolingLib.git",
-            exact: "1.0.0"
+            branch: "static"
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyPluginForAbstraction", dependencies: [.product(name: "ReusableToolingLib", package: "iOSReusableToolingLib"),],
+            name: "MyPluginForAbstraction", dependencies: [.product(name: "ReusableToolingLib_Static", package: "iOSReusableToolingLib"),],
             path: "MyPluginForAbstraction/Sources/MyPluginForAbstraction"
         ),
         .testTarget(
